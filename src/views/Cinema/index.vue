86e144a129044b88b4f0c977ce3b93fd9c7cb7cb
jiucheng <15279232381@163.com>
2020-04-03 14:38:43 +0800

    cinema and login

--- src/views/Cinema/index.vue
+++ src/views/Cinema/index.vue
@@ -1,6 +1,20 @@
 <template>
     <div id="main">
         <Header title="喵喵影院"></Header>
+        <div id="content">
+            <div class="cinema_menu">
+                <div class="city_switch">
+				    全城 <i class="iconfont icon-lower-triangle"></i>
+                </div>
+                <div class="brand_swtich">
+                    品牌 <i class="iconfont icon-lower-triangle"></i>
+                </div>
+                <div class="feature_switch">
+                    特色 <i class="iconfont icon-lower-triangle"></i>
+                </div>
+            </div>
+            <CinemaList></CinemaList>
+        </div>
         <TabBar></TabBar>
     </div>
 </template>
@@ -8,15 +22,18 @@
 <script>
 import Header from '@/components/Header';
 import TabBar from '@/components/TabBar';
+import CinemaList from '@/components/CinemaList'
 export default {
     name: 'Cinema',
     components: {
         Header,
-        TabBar
+        TabBar,
+        CinemaList
     }
 }
 </script>
 
 <style scoped>
-
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+#content .cinema_menu.router-link-active{  color: #ef4238; border-bottom: 2px #ef4238 solid; }
 </style>
